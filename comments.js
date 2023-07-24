d4cd63f35819ad0bf696912500f4dfd6c44eb0b8
elecsense <90675810+elecsense@users.noreply.github.com>
2023-07-24 10:09:21 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,70 +1,61 @@
 //create web server
-//create web server
-var express = require('express');
-var router = express.Router();
-var path = require('path');
-var bodyParser = require('body-parser');
-var fs = require('fs');
-var app = express();
-var util = require('util');
-var multer = require('multer');
-var upload = multer({ dest: 'uploads/' });
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var db = mongoose.connection;
-//connect to MongoDB
-mongoose.connect('mongodb://localhost/Comments');
-//create schema
-var commentSchema = new Schema({
-    name: String,
-    comment: String
-});
-//create model
-var Comment = mongoose.model('Comment', commentSchema);
-//create a new comment
-var comment1 = Comment({ name: 'Maggie', comment: 'This is a comment' });
-//save comment
-comment1.save(function(err) {
-    if (err) throw err;
-    console.log('Comment saved!');
-});
-//get all comments
-Comment.find({}, function(err, comments) {
-    if (err) throw err;
-    console.log(comments);
-});
-//get one comment
-Comment.find({ name: 'Maggie' }, function(err, comments) {
-    if (err) throw err;
-    console.log(comments);
-});
-//get any comment containing a c
-Comment.find({ comment: /c/ }, function(err, comments) {
-    if (err) throw err;
-    console.log(comments);
-});
-//get any comment with a name starting with M
-Comment.find({ name: /^M/ }, function(err, comments) {
-    if (err) throw err;
-    console.log(comments);
-});
-//get any comment with a name starting with M
-Comment.find({ name: /^M/ }, function(err, comments) {
-    if (err) throw err;
-    console.log(comments);
-});
-//get any comment with a name starting with M
-Comment.find({ name: /^M/ }, function(err, comments) {
-    if (err) throw err;
-    console.log(comments);
-});
-//get any comment with a name starting with M
-Comment.find({ name: /^M/ }, function(err, comments) {
-    if (err) throw err;
-    console.log(comments);
-});
-//get any comment with a name starting with M
-Comment.find({ name: /^M/ }, function(err, comments) {
-    if (err) throw err;
+const express = require('express');
+const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
+//const passport = require('passport');
+const Comment = require('../models/comment');
+const User = require('../models/user');
+const mongoose = require('mongoose');
 
+// router.post('/add', passport.authenticate('jwt', { session: false }), (req, res, next) => {
+//     let newComment = new Comment({
+//         _id: new mongoose.Types.ObjectId(),
+//         text: req.body.text,
+//         user: req.user._id
+//     });
+//     newComment.save((err, comment) => {
+//         if (err) {
+//             res.json({ success: false, msg: 'Failed to add comment' });
+//         } else {
+//             res.json({ success: true, msg: 'Comment added' });
+//         }
+//     });
+// });
 
+router.post('/add', (req, res, next) => {
+    let newComment = new Comment({
+        _id: new mongoose.Types.ObjectId(),
+        text: req.body.text,
+        user: req.body.user
+    });
+    newComment.save((err, comment) => {
+        if (err) {
+            res.json({ success: false, msg: 'Failed to add comment' });
+        } else {
+            res.json({ success: true, msg: 'Comment added' });
+        }
+    });
+});
+
+router.get('/all', (req, res, next) => {
+    Comment.find({}, (err, comments) => {
+        if (err) {
+            res.json({ success: false, msg: 'Failed to get comments' });
+        } else {
+            res.json({ success: true, comments: comments });
+        }
+    }).populate('user');
+});
+
+router.post('/delete', (req, res, next) => {
+    Comment.findByIdAndRemove(req.body._id, (err, comment) => {
+        if (err) {
+            res.json({ success: false, msg: 'Failed to delete comment' });
+        } else {
+            res.json({ success: true, msg: 'Comment deleted' });
+        }
+    });
+});
+
+module.exports = router;
