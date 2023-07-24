bddf2e663b49217488f4a5ef8737ffc319ec4835
elecsense <90675810+elecsense@users.noreply.github.com>
2023-07-24 10:12:16 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,61 +1,74 @@
-//create web server
+//create a web server
 const express = require('express');
-const router = express.Router();
-const jwt = require('jsonwebtoken');
-const config = require('../config/database');
-//const passport = require('passport');
-const Comment = require('../models/comment');
-const User = require('../models/user');
+const app = express();
+//connect to mongoDB
 const mongoose = require('mongoose');
+//require the model
+const Comment = require('./models/comment');
+//require the seed file
+const seedDB = require('./seed');
+//require the body-parser
+const bodyParser = require('body-parser');
+//require the method-override
+const methodOverride = require('method-override');
+//require the express-sanitizer
+const expressSanitizer = require('express-sanitizer');
 
-// router.post('/add', passport.authenticate('jwt', { session: false }), (req, res, next) => {
-//     let newComment = new Comment({
-//         _id: new mongoose.Types.ObjectId(),
-//         text: req.body.text,
-//         user: req.user._id
-//     });
-//     newComment.save((err, comment) => {
-//         if (err) {
-//             res.json({ success: false, msg: 'Failed to add comment' });
-//         } else {
-//             res.json({ success: true, msg: 'Comment added' });
-//         }
-//     });
-// });
+//connect to mongoDB
+mongoose.connect('mongodb://localhost:27017/comments', { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false })
+    .then(() => console.log('Connected to DB!'))
+    .catch(error => console.log(error.message));
+//set the view engine
+app.set('view engine', 'ejs');
+//use the body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+//use the express-sanitizer
+app.use(expressSanitizer());
+//use the method-override
+app.use(methodOverride('_method'));
+//use the seedDB
+seedDB();
 
-router.post('/add', (req, res, next) => {
-    let newComment = new Comment({
-        _id: new mongoose.Types.ObjectId(),
-        text: req.body.text,
-        user: req.body.user
-    });
-    newComment.save((err, comment) => {
-        if (err) {
-            res.json({ success: false, msg: 'Failed to add comment' });
+//RESTful routes
+//root route
+app.get('/', (req, res) => {
+    res.redirect('/comments');
+});
+//INDEX route
+app.get('/comments', (req, res) => {
+    Comment.find({}, (error, comments) => {
+        if (error) {
+            console.log(error);
         } else {
-            res.json({ success: true, msg: 'Comment added' });
+            res.render('index', { comments: comments });
         }
     });
 });
-
-router.get('/all', (req, res, next) => {
-    Comment.find({}, (err, comments) => {
-        if (err) {
-            res.json({ success: false, msg: 'Failed to get comments' });
-        } else {
-            res.json({ success: true, comments: comments });
-        }
-    }).populate('user');
+//NEW route
+app.get('/comments/new', (req, res) => {
+    res.render('new');
 });
-
-router.post('/delete', (req, res, next) => {
-    Comment.findByIdAndRemove(req.body._id, (err, comment) => {
-        if (err) {
-            res.json({ success: false, msg: 'Failed to delete comment' });
+//CREATE route
+app.post('/comments', (req, res) => {
+    req.body.comment.body = req.sanitize(req.body.comment.body);
+    Comment.create(req.body.comment, (error, newComment) => {
+        if (error) {
+            console.log(error);
         } else {
-            res.json({ success: true, msg: 'Comment deleted' });
+            res.redirect('/comments');
         }
     });
 });
-
-module.exports = router;
+//SHOW route
+app.get('/comments/:id', (req, res) => {
+    Comment.findById(req.params.id, (error, foundComment) => {
+        if (error) {
+            console.log(error);
+        } else {
+            res.render('show', { comment: foundComment });
+        }
+    });
+});
+//EDIT route
+app.get('/comments/:id/edit', (req, res) => {
+    Comment.findById(req.params.id, (error, found
